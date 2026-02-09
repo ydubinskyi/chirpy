dec6ee534509a4417b43aef0a8a042a3e3947875
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2026-02-09 22:42:15 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ydubinskyi/chirpy
+
+go 1.25.7
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
